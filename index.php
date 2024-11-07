8ea0539545fa55f337db0809cccc7ccfbd5e89aa
Doriane Lucas <doriane.mlcs@gmail.com>
2024-11-07 12:24:55 +0100

    étape 4 : liste des photos sur page accueil

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
-<?php get_header(); ?>
-<h1>Nathalie Mota by Doriane</h1>
-<?php get_footer(); ?>
+<?php
+get_header(); // Inclut l'en-tête du thème
+?>
+
+<main id="primary" class="site-main">
+
+    <?php
+    get_template_part( 'templates-part/hero-header' );
+    ?>
+
+
+            <?php
+            // Arguments pour la requête
+            $args = array(
+            'post_type' => 'photo', 
+            'posts_per_page' => 8, // Nombre de photos à afficher
+            );
+
+            // La requête
+            $photos_query = new WP_Query($args);
+
+            // Boucle
+        if ($photos_query->have_posts()) : ?>
+            <div class="photo-gallery">
+                <?php while ($photos_query->have_posts()) : $photos_query->the_post(); ?>
+                    
+                    <?php get_template_part('templates-part/photo', 'block'); ?>
+                <?php endwhile; ?>
+            </div>
+
+            <?php wp_reset_postdata(); // Réinitialise la requête principale ?>
+
+                <?php else : ?>
+                    <p>Aucune photo à afficher.</p>
+                <?php endif;?>
+</main>
+
+<?php
+get_footer(); // Inclut le pied de page du thème
+?>
